dd3dd97fdf6d66029d31df8212670319a1425dce
sohyeonn <limsohyeonn.18@gmail.com>
2025-05-01 09:59:49 +0900

    js 추가

---
+++ src/main/resources/static/js/member/userRequestHistory.js
@@ -0,0 +1,6 @@
+
+	 document.addEventListener('DOMContentLoaded', function() {
+		 //메뉴 고정
+		 $(".menu").children().eq(3).find('a').css({'margin-left':'5px', 'font-weight':'bold'});
+		 $(".menu").children().eq(3).find('img').css({'display':'inline'});
+	 });
